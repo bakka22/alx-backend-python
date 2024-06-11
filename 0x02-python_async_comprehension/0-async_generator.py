dadcf3d16ffebfdc06cbc1b6d65d83b26818d418
bakka22 <abobakereltom@gmail.com>
2024-06-11 20:43:51 +0200

    Task #0

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 """ async generator """
 import asyncio
-from typing import AsyncGenerator
+import typing
 import random
 
 
-async def async_generator() -> Generator[int, None, None]:
+async def async_generator() -> typing.Generator[int, None, None]:
     """ yields a random int every 1 second """
     for i in range(10):
         await asyncio.sleep(1)
