abbf9433935dca3af3b04a643f0394df5c281385
bakka22 <abobakereltom@gmail.com>
2024-08-02 01:02:04 +0200

    s

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """ type annotations """
-from typing import List
+from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: List[int | float]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """ return the sum of all of the list elements """
     return sum(mxd_lst)
