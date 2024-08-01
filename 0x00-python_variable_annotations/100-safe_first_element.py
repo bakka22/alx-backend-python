b177d1f174b5a2641f0f5f992639308ec715f7b9
bakka22 <abobakereltom@gmail.com>
2024-08-02 01:33:05 +0200

    s

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """ type annotations """
-from typing import Any, Union
+from typing import Any, Union, Sequence
 
 
-def safe_first_element(lst: Union[Any, None]):
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
     """ safely return the first element of a list """
     if lst:
         return lst[0]
