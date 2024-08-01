83d399f1c2bc48aa4ad30b9f6006acb7e493564d
bakka22 <abobakereltom@gmail.com>
2024-08-02 00:56:10 +0200

    s

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ type annotations """
+from typing import List
 
-
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """ return the sum of all of the list elements """
     return sum(input_list)
