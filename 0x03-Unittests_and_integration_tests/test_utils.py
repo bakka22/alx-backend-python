70b31881c6752ef13cfe9c9b7ca43d5a8dc60746
bakka22 <abobakereltom@gmail.com>
2024-08-25 08:30:53 +0200

    s

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -2,7 +2,9 @@
 """ test cases for utils module """
 import unittest
 from parameterized import parameterized
-from utils import access_nested_map
+from utils import access_nested_map, get_json
+from unittest.mock import patch
+import requests
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -15,18 +17,24 @@ class TestAccessNestedMap(unittest.TestCase):
         """ test normal range inputs for utils.access_nested_map """
         self.assertEqual(access_nested_map(mapped, path), expected)
 
-    """@parameterized.expand([
-        ({}, ("a",), KeyError),
-        ({"a": 1}, ("a", "b"), KeyError)])
-    def test_access_nested_map_exeption(self, mapped, path, exception):
-        test invalid inputs
-        with self.assertRaises(exception) as e:
-            access_nested_map(mapped, path)"""
-
     @parameterized.expand([({}, ("a",)), ({"a": 1}, ("a", "b"))])
     def test_access_nested_map_exception(self,  nested_map, path):
-        """Method to test that access_nested_map handles errors correctly"""
+        """ test invalid inputs """
 
         with self.assertRaises(KeyError) as err:
             access_nested_map(nested_map, path)
         self.assertEqual(err.exception.args[0], path[-1])
+
+
+class TestGetJson(unittest.TestCase):
+    """ test cases for utils.get_json """
+
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})])
+    @patch('test_utils.requests.get')
+    def test_get_json(self, url, payload, mock_get):
+        """ test functionality with normal values """
+        mock_get.return_value.json.return_value = payload
+        self.assertEqual(get_json(url), payload)
+        mock_get.assert_called_once_with(url)
