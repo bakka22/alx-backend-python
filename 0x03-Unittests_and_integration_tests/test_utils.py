b152dbdb8492ff17d26b420b4bbdc01b970e361c
bakka22 <abobakereltom@gmail.com>
2024-08-25 06:14:35 +0200

    s

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -12,5 +12,14 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2)])
     def test_access_nested_map(self, mapped, path, expected):
-        """ test cases for utils.access_nested_map """
+        """ test normal range inputs for utils.access_nested_map """
         self.assertEqual(access_nested_map(mapped, path), expected)
+
+    @parameterized.expand([
+        ({}, ("a",), "a"),
+        ({"a": 1}, ("a", "b"), "b")])
+    def test_access_nested_map_exeption(self, mapped, path, expected):
+        """ test invalid inputs """
+        with self.assertRaises(KeyError) as e:
+            access_nested_map(mapped, path)
+        self.assertEqual(e.exception.args[0], expected)
